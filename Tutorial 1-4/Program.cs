97ae530ae9309241bcb9bd7283fb2b742c37ee30
ASUS <ASUS@KUSHANI>
2024-02-11 12:56:49 +0530

    Add project files.

---
+++ Tutorial 1-4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first N terms of the Fibonacci Series: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First " + n + " terms of the Fibonacci Series");
+            Console.ReadLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(FibonacciRecursive(i) +" ");
+                Console.ReadLine();
+
+            }
+        }
+        static int FibonacciRecursive(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+    }
+}
---
+++ Tutorial 1-4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tutorial 1-4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tutorial 1-4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be7b34f7-13ab-4be4-b81d-074eca76e8f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
